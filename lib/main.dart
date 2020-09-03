db7958557c62889236c2ec560b2ab43c886812b8
Yousef Roshandel <yousef.r1999@gmail.com>
2020-09-03 16:55:00 +0430

    generate weather cubit

--- lib/main.dart
+++ lib/main.dart
@@ -13,30 +13,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-class MyChangeNotifier extends ChangeNotifier {
-  int field1;
-  String field2;
-
-  void changeState() {
-    field2 = 'New Value';
-    notifyListeners();
-  }
-}
-
-class MyState {
-  final int field1;
-  final String field2;
-
-  MyState(this.field1, this.field2);
-
-  // = and hashcode overrides...
-}
-
-class MyCubit extends Cubit<MyState> {
-  MyCubit() : super(MyState(0, 'Initial value'));
-
-  void changeState() {
-    emit(MyState(0, 'New Value'));
-  }
-}
