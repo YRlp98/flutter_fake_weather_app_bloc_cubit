d620a652b7f23c0d2af413867d0bfa026ee4088a
Yousef Roshandel <yousef.r1999@gmail.com>
2020-09-03 17:54:41 +0430

    generate WeatherError

--- lib/cubit/weather_state.dart
+++ lib/cubit/weather_state.dart
@@ -13,6 +13,21 @@ class WeatherLoading extends WeatherState {
   const WeatherLoading();
 }
 
+class WeatherLoaded extends WeatherState {
+  final String message;
+  const WeatherLoaded(this.message);
+
+  @override
+  bool operator ==(Object o) {
+    if (identical(this, o)) return true;
+
+    return o is WeatherLoaded && o.message == message;
+  }
+
+  @override
+  int get hashCode => message.hashCode;
+}
+
 class WeatherError extends WeatherState {
   final String message;
   const WeatherError(this.message);
