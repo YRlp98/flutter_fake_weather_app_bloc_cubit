b43056bf813387d33d1a95914ffd532d79c52642
Yousef Roshandel <yousef.r1999@gmail.com>
2020-09-03 17:49:39 +0430

    generate equality

--- lib/cubit/weather_state.dart
+++ lib/cubit/weather_state.dart
@@ -1,6 +1,29 @@
 part of 'weather_cubit.dart';
 
 @immutable
-abstract class WeatherState {}
+abstract class WeatherState {
+  const WeatherState();
+}
 
-class WeatherInitial extends WeatherState {}
+class WeatherInitial extends WeatherState {
+  const WeatherInitial();
+}
+
+class WeatherLoading extends WeatherState {
+  const WeatherLoading();
+}
+
+class WeatherError extends WeatherState {
+  final String message;
+  const WeatherError(this.message);
+
+  @override
+  bool operator ==(Object o) {
+    if (identical(this, o)) return true;
+
+    return o is WeatherError && o.message == message;
+  }
+
+  @override
+  int get hashCode => message.hashCode;
+}
