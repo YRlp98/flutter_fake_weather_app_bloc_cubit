a2d66bbf093c65b700192ba1f4c27218562ff59c
Yousef Roshandel <yousef.r1999@gmail.com>
2020-09-10 19:22:16 +0430

    wrap home with BlocProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_cubit_bloc_tutorial/cubit/weather_cubit.dart';
+import 'package:flutter_cubit_bloc_tutorial/data/weather_repository.dart';
 import 'package:flutter_cubit_bloc_tutorial/pages/weather_search_page.dart';
 import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,8 +11,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
-      home: WeatherSearchPage(),
-    );
+        title: 'Material App',
+        home: BlocProvider(
+          create: (context) => WeatherCubit(FakeWeatherRepository()),
+          child: WeatherSearchPage(),
+        ));
   }
 }
